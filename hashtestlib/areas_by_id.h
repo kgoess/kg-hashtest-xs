f16e0751d7e8744bab13ec85846addd1b2594c4d
Kevin Goess <knet@goess.org>
2015-02-23 21:34:21 -0800

    move areas_by_id data to separate file

---
+++ hashtestlib/areas_by_id.h
@@ -0,0 +1,10 @@
+
+
+Area area_by_id[] = 
+{
+   { "Area0",     0, "there is no zero" },
+   { "Bay Area",  1, "Go, Giants!" },
+   { "Canada",    2, "Canada is #2!" },
+   { "Manhattan", 3, "I'll take Manhattan" },
+   { "San Diego", 4, "Actually, the Niners are here now" }
+};
